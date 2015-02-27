5f0ebb68e111e75265462da9069728ba788b82c2
amadarain <t.f.octo@gmail.com>
2015-02-27 15:01:10 +0900

    able to copy the formatted URL

--- crx/popup.js
+++ crx/popup.js
@@ -1,15 +1,29 @@
-// タブの切り替えで内容を変更する。
 chrome.tabs.query({"active": true, "currentWindow": true}, function(tabs) {
     var tab = {
         "title": tabs[0].title,
         "url": tabs[0].url
     };
+    // TODO 設定ファイルから読む
+    var list = [
+        {
+            "label": "Markdown",
+            "text": "[" + tab.title + "](" + tab.url + ")"
+        },
+        {
+            "label": "Todoist",
+            "text": "@read " + tab.url + " (" + tab.title + ")"
+        }
+    ];
     console.log('current tab title: ' + tab.title);
-    document.getElementById('text1').value =
-        "[" + tab.title + "](" + tab.url + ")";
-    document.getElementById('text2').value =
-        "@read " + tab.url + " (" + tab.title + ")";
+    // TODO 自動的に生成して挿入する
+    for (var i = 1; i <= 2; i++) {
+        document.getElementById('text' + i).value = list[i-1].text;
+        document.getElementById('button' + i).onclick = function() {
+            document.getElementById('text' + i).select();
+            document.execCommand('copy');
+            // TODO 必要に応じてポップアップを閉じる。
+        };
+    }
 });
 console.log('opened'+ Date());
 
-//javascript:prompt("[title](url)", "["+document.title+"]" + "("+document.URL+")"); void(0)
